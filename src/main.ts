6fde7a995ad4be54a4ce81d8c9bf492ce0500781
Red-Hide <guillaume.ferrand67@gmail.com>
2026-06-11 23:57:38 +0200

    Added seq logging

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { ConfigService } from '@nestjs/config';
 import { createWinstonLogger } from '@dad-group-1/backend-common';
 import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
-import { AgentService } from './agent/agent.service';
 import { RmqUrl } from '@nestjs/microservices/external/rmq-url.interface';
 
 dotenv.config(); // Load environment variables from .env
