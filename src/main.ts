226fc36e09d753aed2e51811aa16d09cef76e87f
Red-Hide <guillaume.ferrand67@gmail.com>
2026-06-11 23:21:00 +0200

    Rabbitmq configuration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { createWinstonLogger } from '@dad-group-1/backend-common';
 import * as dotenv from 'dotenv';
 import { Logger } from '@nestjs/common';
 import { AgentService } from './agent/agent.service';
+import { RmqUrl } from '@nestjs/microservices/external/rmq-url.interface';
 
 dotenv.config(); // Load environment variables from .env
 
@@ -15,20 +16,29 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
 
-  const microserviceHost = configService.get<string>('HOST', '0.0.0.0');
-  const microservicePort = configService.get<number>('PORT', 3008);
   const mcpListenPort = configService.get<number>('MCP_LISTEN_PORT', 3009);
 
   app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
-    options: { host: microserviceHost, port: microservicePort },
+    transport: Transport.RMQ,
+    options: {
+      urls: [
+        {
+          username: configService.getOrThrow<string>('RABBITMQ_USERNAME'),
+          password: configService.getOrThrow<string>('RABBITMQ_PASSWORD'),
+          hostname: configService.getOrThrow<string>('RABBITMQ_HOST'),
+          port: configService.getOrThrow<number>('RABBITMQ_PORT'),
+        } as RmqUrl,
+      ],
+      queue: `AGENT_SERVICE_QUEUE`,
+      queueOptions: {
+        durable: true,
+      },
+    },
   });
 
   await app.startAllMicroservices();
   await app.listen(mcpListenPort); // ✅ HTTP server is now up
 
-  new Logger('Bootstrap').log(
-    `AI Agent started — TCP: ${microserviceHost}:${microservicePort}, MCP HTTP: ${mcpListenPort}`,
-  );
+  new Logger('Bootstrap').log(`AI Agent started - MCP HTTP: ${mcpListenPort}`);
 }
 bootstrap();
